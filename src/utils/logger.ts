4900eadef4a6e2dad0c7d1efd0b32edafbd7d709
Brandon Reyes <brareyesb15@gmail.com>
2024-12-11 12:24:20 -0500

    /codegpt

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,6 +2,8 @@ export const log = (message: string) => {
   console.log(message);
 };
 
+// un comment
+
 export const errorHandler = (error: any) => {
   console.error("An error occurred:", error);
 };
