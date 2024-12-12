bdefdcb9ec5a05e27652f20fed915a223fc8b96b
Brandon Reyes <brareyesb15@gmail.com>
2024-12-11 21:25:33 -0500

    a

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,6 +2,8 @@ export const log = (message: string) => {
   console.log(message);
 };
 
+// a comment
+
 export const errorHandler = (error: any) => {
   console.error("An error occurred:", error);
 };
