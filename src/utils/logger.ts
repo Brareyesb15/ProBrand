99c48edde6dc971a5cd7f23574c3abddf2b21e3a
Brandon Reyes <brareyesb15@gmail.com>
2024-12-11 21:22:44 -0500

    en beta, descomentamos log

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,6 +1,6 @@
-// export const log = (message: string) => {
-//   console.log(message);
-// };
+export const log = (message: string) => {
+  console.log(message);
+};
 
 export const errorHandler = (error: any) => {
   console.error("An error occurred:", error);
