40e6fc129f8c734820896da833ef752506de7b97
Brandon Reyes <brareyesb15@gmail.com>
2025-01-07 18:08:04 -0500

    ccc

---
+++ fuente/script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("myForm");
+  const errorMessage = document.getElementById("error_message");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault(); // Evita que el formulario se envíe
+    const inputText = document.getElementById("input_t").value;
+
+    // Validación simple
+    if (inputText === "") {
+      errorMessage.textContent = "El campo de texto está vacío.";
+    } else {
+      errorMessage.textContent = "";
+      alert("Formulario enviado correctamente");
+    }
+  });
+});
