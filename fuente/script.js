fe476091cfc8020ca7282b7f5b2fa95d4fde3cd2
Brandon Reyes <brareyesb15@gmail.com>
2025-01-08 08:57:14 -0500

    ccc

--- fuente/script.js
+++ fuente/script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
   form.addEventListener("submit", (event) => {
     event.preventDefault(); // Evita que el formulario se envíe
 
-    const inputText = document.getElementById("input_text").value;
+    const inputText = document.getElementById("input_").value;
 
     // Validación simple
     if (inputText === "") {
