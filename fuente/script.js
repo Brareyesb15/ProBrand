7b49684373f85d7c41b20103d01af22c028252b9
Brandon Reyes <brareyesb15@gmail.com>
2025-01-14 09:05:34 -0500

    aa

--- fuente/script.js
+++ fuente/script.js
@@ -6,7 +6,6 @@ document.addEventListener("DOMContentLoaded", () => {
     event.preventDefault(); // Evita que el formulario se envíe
     const inputText = document.getElementById("input_text").value;
 
-    // Validación simple
     if (inputText === "") {
       errorMessage.textContent = "El campo de texto está vacío.";
     } else {
