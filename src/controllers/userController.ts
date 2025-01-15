7a6042652d738da2ccde4a5dc33a36ce4e31fcb4
Brandon Reyes <brareyesb15@gmail.com>
2025-01-15 11:10:43 -0500

    15-1- 3

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -6,7 +6,7 @@ export const userController = () => {
   try {
     log("User Controller Initialized");
     const user = getUser();
-    if (validateUser(user)) {
+    if (validateUse(user)) {
       log("User is valid");
     } else {
       log("User is invalid");
