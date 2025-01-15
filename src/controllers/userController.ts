6a7751522f298160d73aa2afbd4b867c4a660056
Brandon Reyes <brareyesb15@gmail.com>
2025-01-15 17:23:12 -0500

    portador

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -4,7 +4,7 @@ import { getUser } from "../services/userService";
 
 export const userController = () => {
   try {
-    log("User Controller Initializsd");
+    log("User Controller Initializszd");
     const user = getUser();
     if (validateUser(user)) {
       log("User is valid");
