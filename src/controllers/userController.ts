8bde2d490abf564287c2b5c27497ea6708e8003f
Brandon Reyes <brareyesb15@gmail.com>
2025-01-15 16:18:31 -0500

    5

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -4,7 +4,7 @@ import { getUser } from "../services/userService";
 
 export const userController = () => {
   try {
-    log("User Controller Initialized");
+    log("User Controller Initializd");
     const user = getUser();
     if (validateUser(user)) {
       log("User is valid");
