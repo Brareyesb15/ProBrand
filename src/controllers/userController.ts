5f2b4f4c514fb0661884bb04c27f1d98e46a4414
Brandon Reyes <brareyesb15@gmail.com>
2025-01-16 11:02:36 -0500

    prueba 2

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,16 +2,15 @@ import { log, errorHandler } from "../utils/logger";
 import { validateUser } from "../utils/validator";
 import { getUser } from "../services/userService";
 
-// export const userController = () => {
-//   try {
-//     log("User Controller Initialized");
-//     const user = getUser();
-//     if (validateUser(user)) {
-//       log("User is valid");
-//     } else {
-//       log("User is invalid");
-//     }
-//   } catch (error) {
-//     errorHandler(error);
-//   }
-// };
+export const userController = () => {
+  try {
+    log("User Controller Initialized");
+    if (validateUser(user)) {
+      log("User is valid");
+    } else {
+      log("User is invalid");
+    }
+  } catch (error) {
+    errorHandler(error);
+  }
+};
