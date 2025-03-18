fdd7a69fb7e7a495d51ba6f73ad7a0d822913923
Brandon Reyes <brareyesb15@gmail.com>
2025-03-17 22:49:31 -0500

    fixbeta 3

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,5 +2,16 @@ import { log, errorHandler } from "../utils/logger";
 import { validateUser } from "../utils/validator";
 import { getUser } from "../services/userService";
 
-export const userController = () => {};
-// solossss
+export const userController = () => {
+  try {
+    log("User Controller Initialized");
+    const user = getUser();
+    if (validateUser(user)) {
+      log("User is valid")
+    } else {
+      log("User is invalid");
+    }
+  } catch (error) {
+    errorHandler(error);
+  }
+};
