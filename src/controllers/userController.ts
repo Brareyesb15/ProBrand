76a64fd0aae1a635be6d442ecf0844af42fd4645
Brandon Reyes <brareyesb15@gmail.com>
2025-03-17 22:50:04 -0500

    fixbeta 3 4

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -7,7 +7,7 @@ export const userController = () => {
     log("User Controller Initialized");
     const user = getUser();
     if (validateUser(user)) {
-      log("User is va")
+      log("User is v3")
     } else {
       log("User is invalid");
     }
